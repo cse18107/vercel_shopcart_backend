39b5d39c3534efdc3787c8367d3b579858c71350
cse18107 <cse18107@cemk.ac.in>
2022-04-28 11:54:40 +0530

    add

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ const dotenv = require('dotenv');
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
 app.use(fileUpload());
-app.set('trust proxy', 1)
 
 
 // Route Imports
